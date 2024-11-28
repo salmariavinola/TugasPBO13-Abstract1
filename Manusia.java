a012a990d154259447cc060e8a5ff2474c18ba92
salmariavinola <108047293+salmariavinola@users.noreply.github.com>
2024-11-28 09:07:45 +0700

    Create Manusia.java

---
+++ Manusia.java
@@ -0,0 +1,12 @@
+class Manusia extends MakhlukHidup {
+
+    @Override
+    String bernafas() {
+        return "Manusia bernafas menggunakan paru-paru.";
+    }
+
+    @Override
+    String berkembangBiak() {
+        return "Manusia berkembang biak secara melahirkan (vivipar).";
+    }
+}
