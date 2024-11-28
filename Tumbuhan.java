485806f63b9392154837f5611ad45f50c5bcb072
salmariavinola <108047293+salmariavinola@users.noreply.github.com>
2024-11-28 09:08:07 +0700

    Create Tumbuhan.java

---
+++ Tumbuhan.java
@@ -0,0 +1,12 @@
+class Tumbuhan extends MakhlukHidup {
+
+    @Override
+    String bernafas() {
+        return "Tumbuhan bernafas melalui stomata atau lentisel.";
+    }
+
+    @Override
+    String berkembangBiak() {
+        return "Tumbuhan berkembang biak melalui biji, tunas, atau cangkok.";
+    }
+}
