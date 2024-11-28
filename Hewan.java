e567e880f49d017f3e84039aa095ab1cc70322f8
salmariavinola <108047293+salmariavinola@users.noreply.github.com>
2024-11-28 09:07:24 +0700

    Create Hewan.java

---
+++ Hewan.java
@@ -0,0 +1,12 @@
+class Hewan extends MakhlukHidup {
+
+    @Override
+    String bernafas() {
+        return "Hewan bernafas menggunakan paru-paru atau insang tergantung jenisnya.";
+    }
+
+    @Override
+    String berkembangBiak() {
+        return "Hewan berkembang biak secara bertelur (ovipar) atau melahirkan (vivipar).";
+    }
+}
